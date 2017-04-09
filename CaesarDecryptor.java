1eca8add305a09a8f72b7abdddf20f787079f444
jayala-29 <jayala@ucsd.edu>
2017-04-08 21:24:07 -0700

    Decrypts messages encrypted with Caesar cipher

---
+++ CaesarDecryptor.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.lang.StringBuilder;
+public class CaesarDecryptor {
+  public static void main(String[] args) {
+    StringBuilder decryptedMessage = new StringBuilder();
+    Scanner input = new Scanner(System.in);
+    System.out.print("Please enter a message to decrypt: ");
+    String message = input.nextLine();
+    System.out.print("Please enter the letter key: ");
+    String key = input.nextLine();
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+    for(int i = 0; i < message.length(); i++) {
+      if (message.charAt(i) == ' ') {
+        decryptedMessage.append(" ");
+      }
+      else {
+        char c = (char) (((message.charAt(i) - 97 - (key.charAt(0) - 97)) % 26) + 97);
+        decryptedMessage.append(String.valueOf(c));
+      }
+    }
+    System.out.print("Decrypted message: " + decryptedMessage);
+  }
+}
